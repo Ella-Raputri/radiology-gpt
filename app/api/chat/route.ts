326ce50a38b1a799292673007b812a0825ac3be2
Ella R <ellaraputri@gmail.com>
2025-09-23 00:32:44 +0700

    fix: update qwen model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -154,7 +154,7 @@ export async function POST(req: Request) {
       const llmModel = (() => {
         switch (llm) {
           case 'qwen-2-5-72b':
-            return "qwen/qwen2.5-vl-72b-instruct:free"; //qwen
+            return "qwen/qwen-2.5-72b-instruct:free"; //qwen
           default:
             return "deepseek/deepseek-chat-v3.1:free"; //deepseek V3
         }
